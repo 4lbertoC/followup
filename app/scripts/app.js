1e01a1dfb723ea1f9b04adb7591d1ccab5351ae4
Alberto Congiu <4lbertoC@gmail.com>
2014-10-12 04:55:33 +0200

    added scaffolding to main page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 (function(document) {
     'use strict';
-    var pages = document.querySelector('core-pages');
+    var scaffoldTemplate = document.querySelector('scaffold-template');
     var events = document.querySelector('#events');
     var ajax = document.querySelector('core-ajax');
     var questionList = document.querySelector('question-list');
@@ -15,32 +15,32 @@
 
     });
     document.querySelector('#new-event-button').addEventListener('click', function(e) {
-        window.open('publish/?userID=22','_self');
+        window.open('publish/?userID=22', '_self');
     });
 
     window.FollowUp = {};
 
     FollowUp.openQuestionList = function(eventID) {
-        pages.selected = 2;
+
+        scaffoldTemplate.setPage(2);
         questionList.eventID = eventID;
         history.pushState({
-        	eventID: eventID
+            eventID: eventID
         }, 'FollowUp', '/event/' + eventID);
     };
 
     window.addEventListener('popstate', function(data) {
-    	var eventIDregex = /.*\/event\/([0-9]+)/
-    	var match = document.URL.match(eventIDregex);
-    	if(match) {
-    		pages.selected = 2;
-    		var eventID = match[1];
-	        pages.selected = 2;
-	        questionList.eventID = eventID;
-    	} else {
-    		pages.selected = 0;
-    	}
+        var eventIDregex = /.*\/event\/([0-9]+)/
+        var match = document.URL.match(eventIDregex);
+        if (match) {
+            scaffoldTemplate.setPage(2);
+            var eventID = match[1];
+            questionList.eventID = eventID;
+        } else {
+            scaffoldTemplate.setPage(0);
+        }
     });
-    
+
     // wrap document so it plays nice with other libraries
     // http://www.polymer-project.org/platform/shadow-dom.html#wrappers
 })(wrap(document));
